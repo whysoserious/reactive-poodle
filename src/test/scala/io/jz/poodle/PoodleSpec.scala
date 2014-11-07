10e234b2bc9e421a209dd6df1e3e2aca66ca9cba
Jan <janek.ziniewicz@gmail.com>
2014-11-07 19:04:15 +0100

    Generate random story id - tests

---
+++ src/test/scala/io/jz/poodle/PoodleSpec.scala
@@ -0,0 +1,25 @@
+package io.jz.poodle
+
+import io.jz.poodle.poodle.Poodle
+import org.scalatest.{Matchers, FlatSpec}
+
+import scala.util.Random
+
+
+class PoodleSpec extends FlatSpec with Matchers {
+
+  import Poodle._
+
+  "Poodle" should "generate random storyId" in {
+    val randomStoryId = randomStoryIdFun(1, 10)(new Random(1))
+    randomStoryId() should equal (7)
+    randomStoryId() should equal (2)
+    randomStoryId() should equal (2)
+  }
+
+  it should "asdfasdf" in {
+    assert(true)
+  }
+
+
+}
