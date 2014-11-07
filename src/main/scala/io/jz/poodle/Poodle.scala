f9c91eb3b9fb07383efcbe529dbe2b984ebf2929
Jan <janek.ziniewicz@gmail.com>
2014-11-07 20:18:23 +0100

    Extract implicits

--- src/main/scala/io/jz/poodle/Poodle.scala
+++ src/main/scala/io/jz/poodle/Poodle.scala
@@ -10,6 +10,8 @@ import org.json4s.jackson.Serialization.{read, write}
 
 object Poodle {
 
+  implicit val formats: Formats = Serialization.formats(NoTypeHints)
+
   case class Location(storyId: Int, commentPage: Int, commentId: String)
 
   case class Chunk(payload: Array[Byte], parentLocation: Option[Location])
@@ -24,11 +26,11 @@ object Poodle {
   }
 
   //serialize chunk
-  def serializeChunk(chunk: Chunk)(implicit formats: Formats = Serialization.formats(NoTypeHints)): String = {
+  def serializeChunk(chunk: Chunk)(implicit formats: Formats): String = {
     write(chunk)
   }
 
-  def deserializeChunk(str: String)(implicit formats: Formats = Serialization.formats(NoTypeHints)): Chunk = {
+  def deserializeChunk(str: String)(implicit formats: Formats): Chunk = {
     read[Chunk](str)
   }
 
@@ -39,7 +41,7 @@ object Poodle {
   // create chunk with location
 
   // encode chunk with SHA-1
-  def encryptSha1Fun(salt: String): String => Array[Byte] = {
+  def encrypt(salt: String): String => Array[Byte] = {
     input =>
       val md: MessageDigest = MessageDigest.getInstance("SHA")
       md.digest((salt + input).getBytes)
